582c55cf8ca68ed1bde51abf6ec4aad9486dd57c
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2023-01-10 14:28:53 +0100

    Adding products and images

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -35,8 +35,12 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(tablet);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Britney Spears' chewing gum", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+        productDataStore.add(new Product("Pamela Anderson's Baywatch swimsuit", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+        productDataStore.add(new Product("Harrison Ford's Indiana Jones whip", new BigDecimal("89"), "USD", "Amazon's latest.", tablet, amazon));
+        productDataStore.add(new Product("Billie Eilish's outgrown clothes", new BigDecimal("89"), "USD", "Amazon...", tablet, amazon));
+        productDataStore.add(new Product("Michael Jackson's nose", new BigDecimal("89"), "USD", "Amazon...", tablet, amazon));
+        productDataStore.add(new Product("David Beckham's underpants", new BigDecimal("89"), "USD", "Amazon...", tablet, amazon));
+
     }
 }
