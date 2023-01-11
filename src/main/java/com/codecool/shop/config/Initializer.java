d1cb7d106cb775f2d0d00663095560f838c55788
dettylestar <lestardetty@gmail.com>
2023-01-11 13:34:35 +0100

    category filtering under construction

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -25,22 +25,32 @@ public class Initializer implements ServletContextListener {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        Supplier amazon = new Supplier("Amazon", "Man");
         supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
+        Supplier lenovo = new Supplier("Lenovo", "Woman");
         supplierDataStore.add(lenovo);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
+        ProductCategory britney = new ProductCategory("Britney", "Woman", "Britney's gum is really good if you want to glue something.");
+        productCategoryDataStore.add(britney);
+        ProductCategory pamela = new ProductCategory("Pamela", "Woman", "She wear it for a long time, when the friends went out to her... but she hasn't got friends anymore so Pamela wants to sell it.");
+        productCategoryDataStore.add(pamela);
+        ProductCategory harrison = new ProductCategory("Harrison", "Man", "If you want to surprise your boyfriend/girlfriend you can buy Harrison's whip... You can cosplay him with it.");
+        productCategoryDataStore.add(harrison);
+        ProductCategory billie = new ProductCategory("Billie", "Woman", "Billie wears oversize clothes... so she will never outgrown her clothes... but you can pay a lot of money for it to us, but your order will never deliver   .");
+        productCategoryDataStore.add(billie);
+        ProductCategory michael = new ProductCategory("Michael", "Man", "You need a plastic surgery for nose? AWESOME! You can buy Jacko's nose for yourself for a wonderful price.");
+        productCategoryDataStore.add(michael);
+        ProductCategory david = new ProductCategory("David", "Man", "Do you want an underpants, that had a celebrity's jewelry in it? BUY IT!");
+        productCategoryDataStore.add(david);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Britney Spears' chewing gum", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Pamela Anderson's Baywatch swimsuit", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Harrison Ford's Indiana Jones whip", new BigDecimal("89"), "USD", "Amazon's latest.", tablet, amazon));
-        productDataStore.add(new Product("Billie Eilish's outgrown clothes", new BigDecimal("89"), "USD", "Amazon...", tablet, amazon));
-        productDataStore.add(new Product("Michael Jackson's nose", new BigDecimal("89"), "USD", "Amazon...", tablet, amazon));
-        productDataStore.add(new Product("David Beckham's underpants", new BigDecimal("89"), "USD", "Amazon...", tablet, amazon));
+        productDataStore.add(new Product("Britney Spears' chewing gum", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", britney, amazon));
+        productDataStore.add(new Product("Pamela Anderson's Baywatch swimsuit", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", pamela, lenovo));
+        productDataStore.add(new Product("Harrison Ford's Indiana Jones whip", new BigDecimal("89"), "USD", "Amazon's latest.", harrison, amazon));
+        productDataStore.add(new Product("Billie Eilish's outgrown clothes", new BigDecimal("89"), "USD", "Amazon...", billie, amazon));
+        productDataStore.add(new Product("Michael Jackson's nose", new BigDecimal("89"), "USD", "Amazon...", michael, amazon));
+        productDataStore.add(new Product("David Beckham's underpants", new BigDecimal("89"), "USD", "Amazon...", david, amazon));
 
     }
 }
---
+++ src/main/webapp/static/js/index.js
@@ -0,0 +1,23 @@
+function filterByCategory(categoryName) {
+    let cards = document.querySelectorAll(".card")
+    for (let cardElement of cards) {
+        if(cardElement.dataset.category !== categoryName) {
+            cardElement.setAttribute("display", "None")
+
+        }
+
+    }
+}
+
+function buttonForCategory() {
+    let buttonForWoman = document.querySelector(".btn-for-woman")
+    buttonForWoman.addEventListener("click", () => {
+        filterByCategory("Woman")
+    })
+    let buttonForMan = document.querySelector(".btn-for-man")
+    buttonForMan.addEventListener("click", () => {
+        filterByCategory("Man")
+    })
+}
+
+buttonForCategory()
