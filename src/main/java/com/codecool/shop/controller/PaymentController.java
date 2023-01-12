9d6609336915592e96e154782adcaa2a9f76c306
Zsinka Zoltán <zsinka.zoltan@gmail.com>
2023-01-12 10:31:55 +0100

    payment endpoint and front end almost done.

--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -1,5 +1,14 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.service.ProductService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +21,21 @@ public class PaymentController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Itt határozzuk meg hogy honnan vegye ki az adatokat amiket majd megjelenít
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        System.out.println(productDataStore);
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
 
+        // Ez rajzolja ki a html oldalt
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+
+        //ide kell beadni melyik itemeket dobja be
+        context.setVariable("category", productService.getProductCategory(1));
+        context.setVariable("products", productService.getProductsForCategory(1));
+
+        // Ez futtatja az rendert és hogy mi alapján rendereljen
+        engine.process("product/payment.html", context, response.getWriter());
     }
 }
